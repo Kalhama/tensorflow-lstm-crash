3b6fb8430cc0f145e50a399304beacc13fb5ff54
Max Kalhama <kalhama.max@gmail.com>
2021-01-27 18:55:42 +0200

    Simplify code

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from tensorflow.python.keras import callbacks
 # tf.config.experimental.set_memory_growth(gpu[0], True)
 
 def get_train_data():
-    data_x = []
-    data_y = []
-    for i in range(8000):
-        data_x.append(np.random.rand(200,3))
-        data_y.append(np.random.rand(5,1))
+    while True:
+        data_x = []
+        data_y = []
+        for i in range(100):
+            data_x.append(np.random.rand(200,3))
+            data_y.append(np.random.rand(5,1))
 
-    return np.array(data_x), np.array(data_y)
+        yield np.array(data_x), np.array(data_y)
 
 
 def build_model():
@@ -22,61 +23,28 @@ def build_model():
 
     ### Model with LSTM crashes ###
     model.add(LSTM(200, input_shape=(200,3), return_sequences=True))
-    model.add(Dropout(0.2))
     model.add(LSTM(200, return_sequences=True))
-    model.add(Dropout(0.2))
     model.add(LSTM(200, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(200, return_sequences=True))
-    model.add(Dropout(0.2))
     model.add(LSTM(200, return_sequences=False))
-    model.add(Dropout(0.2))
     model.add(Dense(5, activation='linear'))
 
-    ### Model without LTSM doesn't crash ###
-    # model.add(Flatten(input_shape=(200,3)))
-    # model.add(Dense(500, activation='relu'))
-    # model.add(Dropout(0.2))
-    # model.add(Dense(500, activation='relu'))
-    # model.add(Dropout(0.2))
-    # model.add(Dense(500, activation='relu'))
-    # model.add(Dropout(0.2))
-    # model.add(Dense(500, activation='relu'))
-    # model.add(Dropout(0.2))
-    # model.add(Dense(5, activation='linear'))
+    print(model.summary())
     
-    model.compile(loss='mse', optimizer='adam', metrics=[tf.keras.metrics.CategoricalAccuracy()])
+    model.compile(loss='mse', optimizer='adam')
 
-    # print(model.summary())
-    
     print('[Model] Model Compiled')
 
     return model
 
-def train_model(model, x, y):
-    print('[Model] Training Started')
-
-    model.fit(
-        x,
-        y,
-        epochs=1000,
-        batch_size=32
-    )
-
-    print('[Model] Training Completed')
-
 def main():
-    x, y = get_train_data()
-
     model = build_model()
 
-    train_model(
-        model,
-        x,
-        y
+    model.fit(
+        get_train_data(),
+        epochs=1000,
+        steps_per_epoch=1000,
+        batch_size=100
     )
-
-
 
 if __name__ == '__main__':
     main()
